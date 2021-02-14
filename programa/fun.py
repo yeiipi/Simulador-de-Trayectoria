5945b4ee516cdc043c88f02bb2d4492e709438de
yeiipi <juanpab.sierra@urosario.edu.co>
2021-02-13 21:46:27 -0500

    Agregar display de tiempo
    issue no: UD011

--- programa/fun.py
+++ programa/fun.py
@@ -7,6 +7,8 @@ from math import cos, sin, radians
 g = 9.80665
 
 class Proyectil:
+
+
     def __init__(self,v0:float,theta:float,color:str):
         self.x = 0
         self.y = 0
@@ -18,30 +20,46 @@ class Proyectil:
         self.vx = v0 * cos(radians(theta))
         self.vy = v0 * sin(radians(theta))
 
+
     def __repr__(self):
         return f'({self.x,self.y})'
+
 
     def make_path(self,ax):
         self.path, = ax.plot(self.x,self.y,'ro-',color=f'{self.color}', label=f'{self.theta}°/v:{self.v0}')
         self.line, = ax.plot(self.x,self.y,'r--',color=f'{self.color}')
 
+
     def set_xy(self,nx,ny):
         self.x = nx
         self.y = ny
 
+
     def set_t(self,t):
         self.t = t
 
+
     def update_x(self):
         self.x = 0 + self.vx*(self.t) + 0.5*self.ax*((self.t)**2)
+        for i in range(1,len(self.y)):
+            if self.y[i] == 0:
+                self.x[i] = self.x[i-1]
+
 
     def update_y(self):
         self.y = 0 + self.vy*(self.t) + 0.5*self.ay*((self.t)**2)
+        for i in range(len(self.y)):
+            if self.y[i] < 0:
+                self.y[i] = 0
+
+
+
 
     def update_pos(self,frame):
         self.frame = frame
         self.path.set_data((self.x[frame],self.y[frame]))
         self.line.set_data((self.x[:frame],self.y[:frame]))
+
 
     def make_some_limits(self):
         t_max = 2*self.v0*sin(radians(self.theta))/g
@@ -66,13 +84,14 @@ def HelloWorld(t_max:float,y_max:float,x_max:float):
     ====================
     """
     fig, ax = plt.subplots()
-    ax = plt.axes(autoscale_on=True, xlim=(0,x_max*1.2), ylim=(0,y_max*1.2))
+    ax = plt.axes(autoscale_on=True, xlim=(0,x_max*1.4), ylim=(0,y_max*1.4))
     ax.set_xlabel('distancia en x')
     ax.set_ylabel('distancia en y')
     ax.grid()
 
     t = np.linspace(0,t_max,num=240)
     return fig, ax, t
+
 
 def MAXMAXMAX(proyectiles):
     MAX_T = 0
@@ -90,11 +109,13 @@ def MAXMAXMAX(proyectiles):
     return MAX_T,MAX_Y,MAX_X
 
 
+def el_alma_de_la_fiesta(frame,tiempo,t_total:int,args:list):
 
+    tiempo.set_text(f'T = {frame/t_total}s')
 
-def el_alma_de_la_fiesta(frame,*args):
     for i in args:
         i.update_pos(frame)
+
     return args
 
 
--- programa/main.py
+++ programa/main.py
@@ -3,17 +3,18 @@ from fun import *
 
 def main():
 
-    col1 = [ '#e76f51', '#f4a261', '#e9c46a', '#2a9d8f', '#264653']
-    col2 = [ '#0f4c5c', '#e36414', '#fb8b24', '#9a031e', '#5f0f40']
-    col3 = [ '#ff9b85', '#ffd97d', '#aaf683', '#60d394', '#ee6055']
+    col1 = [ '#e76f51', '#f4a261', '#e9c46a', '#8AB17D', '#2a9d8f', '#264653']
+    col2 = [ '#8CB369', '#C0CB77', '#DAC773', '#f4c26f', '#f4a259', '#d87755']
+    col3 = [ '#40A98C', '#7FC0AA', '#FFDF73', '#F7A04B', '#F95045', '#D7E661']
 
-    p1 = Proyectil(22,15,col2[0])
-    p2 = Proyectil(22,30,col2[1])
-    p3 = Proyectil(22,45,col2[2])
-    p4 = Proyectil(22,60,col2[3])
-    p5 = Proyectil(22,90,col2[4])
+    p1 = Proyectil(22,15,col1[0])
+    p2 = Proyectil(22,30,col1[1])
+    p3 = Proyectil(22,45,col1[2])
+    p4 = Proyectil(22,60,col1[3])
+    p5 = Proyectil(22,75,col1[4])
+    p6 = Proyectil(22,90,col1[5])
 
-    proyectiles = [p1,p2,p3,p4,p5]
+    proyectiles = [p1,p2,p3,p4,p5,p6]
     MAX_T,MAX_Y,MAX_X = MAXMAXMAX(proyectiles)
 
     fig, ax, t = HelloWorld(MAX_T,MAX_Y,MAX_X)
@@ -21,12 +22,14 @@ def main():
 
     for i in proyectiles:
         i.set_t(t)
-        i.update_x()
         i.update_y()
+        i.update_x()
         i.make_path(ax)
 
+    tiempo = ax.text(0.5,MAX_Y*1.25, "T = 0s")
 
-    anim = animation.FuncAnimation(fig,el_alma_de_la_fiesta,frames=len(t),interval=len(t)/24,fargs=proyectiles)
+
+    anim = animation.FuncAnimation(fig,el_alma_de_la_fiesta,frames=len(t),interval=len(t)/24,fargs=[tiempo,len(t)/24,proyectiles])
     plt.legend()
     plt.show()
 
